ebdf3ca5a22be4abbfcdabb858b7efd358ec7c4b
Abhijeet Jain <jain.abhijeet19aj@gmail.com>
2021-07-18 19:39:20 +0530

    Create Binary_serach_iteration.py

---
+++ Code/Searching/Binary_serach_iteration.py
@@ -0,0 +1,19 @@
+def binary_serach(x,s,first,last):
+	while first <= last:
+		mid = (first + last)//2
+		if x[mid] == s:
+			return x[mid]
+			
+		elif x[mid]>s:
+			last = mid -1
+		else:
+			first = mid+1
+	return -1
+
+x = [1,2,3,4,5,6,7]
+s = 20
+first = 0
+last = len(x) - 1
+
+
+print(binary_serach(x,s,first,last))
